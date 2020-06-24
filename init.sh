e960dacebe89de2c75b89f42645df42d8a540bab
hc07180011 <henrychao0718@gmail.com>
2020-06-24 18:19:43 +0800

    final

--- init.sh
+++ init.sh
@@ -1,6 +1,8 @@
 wget -r -np -R "index.html*" 140.112.29.204/os_data/input/
+rm -rf input/
 mv 140.112.29.204/os_data/input ./
 rm -rf 140.112.29.204
 wget -r -np -R "index.html*" 140.112.29.204/os_data/output/
+rm -rf output/
 mv 140.112.29.204/os_data/output ./
 rm -rf 140.112.29.204
